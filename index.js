ad776ba5cdf5b15fb5c19c02d165e931d7e70c27
Pavel <browser091@gmail.com>
2020-02-02 22:41:14 +0300

    task1

---
+++ index.js
@@ -0,0 +1,31 @@
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+let sum = 0
+
+for (let key in salaries) {
+    if (salaries[key].hasOwnProperty) {
+        sum = sum + salaries[key]
+    }
+}
+
+
+// const arrVal = Object.values(salaries)
+
+// for(let i = 0; i < arrVal.length; i++){
+//   sum = sum + arrVal[i]
+// }
+
+// arrVal.forEach((elem)=>{
+//     sum = sum + elem
+// })
+
+
+// sum = Object.values(salaries).reduce((acc, cur) => acc + cur, 0)
+
+
+
+console.log('sum', sum)
